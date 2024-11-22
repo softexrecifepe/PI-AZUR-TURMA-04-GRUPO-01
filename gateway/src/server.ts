7dbef18de2641e625e5d01270eb466010fd5af30
ramgund <vtwolmer9@gmail.com>
2024-11-22 17:43:10 -0300

    Att gateway

--- gateway/src/server.ts
+++ gateway/src/server.ts
@@ -2,15 +2,6 @@ import express from "express";
 import logger from "morgan";
 import helmet from "helmet";
 import httpProxy from "express-http-proxy";
-import path, { resolve } from "path";
-import { readFileSync } from "fs";
-
-const pathFile = resolve(process.cwd(), "config.yml");
-
-// terminar
-const readConfig = readFileSync(pathFile, { encoding: "utf-8" });
-console.log(pathFile);
-console.log(readConfig);
 
 const app = express();
 
@@ -32,6 +23,148 @@ app.use(
   })
 );
 
-// outras rotas
+app.use(
+  "/api/gerenciamento/comprador",
+  httpProxy("http://localhost:3007", {
+    proxyReqPathResolver: (req) => {
+      return req.originalUrl; //
+    },
+  })
+);
 
-app.listen(3002, () => console.log("Servidor rodando!"));
+app.use(
+  "/api/gerenciamento/endereco",
+  httpProxy("http://localhost:3007", {
+    proxyReqPathResolver: (req) => {
+      return req.originalUrl; //
+    },
+  })
+);
+
+app.use(
+  "/api/gerenciamento/generate-pdf",
+  httpProxy("http://localhost:3007", {
+    proxyReqPathResolver: (req) => {
+      return req.originalUrl; //
+    },
+  })
+);
+
+app.use(
+  "/api/gerenciamento/socio",
+  httpProxy("http://localhost:3007", {
+    proxyReqPathResolver: (req) => {
+      return req.originalUrl; //
+    },
+  })
+);
+
+app.use(
+  "/api/gerenciamento/vendedor",
+  httpProxy("http://localhost:3007", {
+    proxyReqPathResolver: (req) => {
+      return req.originalUrl; //
+    },
+  })
+);
+
+app.use(
+  "/api/extracao/aquisicaoImovel",
+  httpProxy("http://localhost:3006", {
+    proxyReqPathResolver: (req) => {
+      return req.originalUrl; //
+    },
+  })
+);
+
+app.use(
+  "/api/extracao/comprador",
+  httpProxy("http://localhost:3006", {
+    proxyReqPathResolver: (req) => {
+      return req.originalUrl; //
+    },
+  })
+);
+
+app.use(
+  "/api/extracao/construtoraFiadora",
+  httpProxy("http://localhost:3006", {
+    proxyReqPathResolver: (req) => {
+      return req.originalUrl; //
+    },
+  })
+);
+
+app.use(
+  "/api/extracao/credora",
+  httpProxy("http://localhost:3006", {
+    proxyReqPathResolver: (req) => {
+      return req.originalUrl; //
+    },
+  })
+);
+
+app.use(
+  "/api/extracao/empreendedorFiador",
+  httpProxy("http://localhost:3006", {
+    proxyReqPathResolver: (req) => {
+      return req.originalUrl; //
+    },
+  })
+);
+
+app.use(
+  "/api/extracao/endereco",
+  httpProxy("http://localhost:3006", {
+    proxyReqPathResolver: (req) => {
+      return req.originalUrl; //
+    },
+  })
+);
+
+app.use(
+  "/api/extracao/imovel",
+  httpProxy("http://localhost:3006", {
+    proxyReqPathResolver: (req) => {
+      return req.originalUrl; //
+    },
+  })
+);
+
+app.use(
+  "/api/extracao/incorporadora",
+  httpProxy("http://localhost:3006", {
+    proxyReqPathResolver: (req) => {
+      return req.originalUrl; //
+    },
+  })
+);
+
+app.use(
+  "/api/extracao/representante",
+  httpProxy("http://localhost:3006", {
+    proxyReqPathResolver: (req) => {
+      return req.originalUrl; //
+    },
+  })
+);
+
+app.use(
+  "/api/extracao/socio",
+  httpProxy("http://localhost:3006", {
+    proxyReqPathResolver: (req) => {
+      return req.originalUrl; //
+    },
+  })
+);
+
+app.use(
+  "/api/extracao/vendedor",
+  httpProxy("http://localhost:3006", {
+    proxyReqPathResolver: (req) => {
+      return req.originalUrl; //
+    },
+  })
+);
+
+app.listen(3001, () => console.log("Servidor rodando!"));
